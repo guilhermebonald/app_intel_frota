6d985f57e62c941e40999d07ff9fd7fd6047f607
guilhermebonald <guilhermebonald@hotmail.com>
2023-03-13 19:48:20 -0300

    Add Functions

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ def add():
     # get input from form with name.
     frota = request.form['frota']
     placa = request.form['placa'].upper()
+
     # append data from input in list.
     add = mongo_connect.db_management()
     add.add_to_db(frota=int(frota), placa=str(placa))
+
     # returning to "home_page" after to add itens in table with the list.
     return redirect(url_for("home_page"))
 
@@ -29,12 +31,32 @@ def add():
 # POST to edit item in list
 @app.route("/editar/<id>", methods=["GET", "POST"])
 def update(id):
-    pass
+    # Get specific car data.
+    item_id = mongo_connect.db_management().get_by_id(str(id))
+
+    if request.method == "POST":
+        # Get Form Data.
+        frota = request.form['update_frota']
+        placa = request.form['update_placa'].upper()
+
+        # item_id (dict) update data.
+        item_id['frota'] = int(frota)
+        item_id['placa'] = placa
+
+        # Set update in DB
+        mongo_connect.db_management().update_data(id)
+
+        # Redirect to page
+        return redirect(url_for("home_page"))
+
+    # Return to update form
+    return render_template("update.html", item=item_id)
 
 @app.route("/deletar/<id>", methods=["GET", "POST"])
 def delete(id):
     mongo_connect.db_management().delete_data(id)
     return redirect(url_for('home_page'))
+
 
 # Main execute
 if __name__ == "__main__":
--- modules/mongo_connect.py
+++ modules/mongo_connect.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 uri = "mongodb://localhost:27017/"
 
@@ -14,25 +15,36 @@ class db_management:
     def get_data(self):
         data = []
         for i in car.find():
-        #    parse = {}
-        #    parse["id"] = str(i["_id"])
-        #    parse["frota"] = int(i["frota"])
-        #    parse["placa"] = str(i["placa"])
-        #    data.append(parse)
             data.append(i)
         return data
+
+    def get_by_id(self, id=str):
+        return car.find_one(
+            {"_id": ObjectId(id)}
+        )
 
     def add_to_db(self, frota=int, placa=str):
         car.insert_one(
             {"frota": frota, "placa": placa}
         )
 
-    def edit_data(self, _id=str):
-        car.find(
-            {"_id": _id}
+    def update_data(self, frota=int, placa=str):
+        # Frota Value Update
+        car.update_one(
+            {"frota": 65},
+            {"$set": {"frota": 76}}
         )
 
-    def delete_data(self, id):
-        car.delete_one(
-            {"_id": id}
+        # Placa Value Update
+        car.update_one(
+            {"placa": "GBB-6423"},
+            {"$set": {"placa": "YGH-8324"}},
         )
+
+    def delete_data(self, id=str):
+        car.delete_one(
+            {"_id": ObjectId(id)}
+        )
+
+
+db_management().update_data()
