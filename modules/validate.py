b2b6144e734bd0537e3e19a21dc09a45a5f063ed
guilhermebonald <guilhermebonald@hotmail.com>
2023-03-18 16:10:31 -0300

    Add new

--- modules/validate.py
+++ modules/validate.py
@@ -4,7 +4,8 @@ from wtforms import Form, StringField, PasswordField, validators, SubmitField
 is a subclass of "Form", so 'add_validate' inherits (herda)
 the attributes for class 'Form'"""
 
-class add_validate(Form): #Form(formdata=request.form[is dict])
+
+class add_validate(Form):  # Form(formdata=request.form[is dict])
     frota = StringField("Frota", [validators.DataRequired()])
     plate = StringField(
-        "Placa", [validators.DataRequired(), validators.Length(max=7, min=6)])
+        "Placa", [validators.DataRequired()])
