33124391360de870403190089595871b8d7ffa47
guilhermebonald <guilhermebonald@hotmail.com>
2023-03-25 17:35:53 -0300

    Add new features

--- main.py
+++ main.py
@@ -1,87 +1,99 @@
 from flask import Flask, render_template, request, redirect, url_for
-from modules import mongo_connect, validate
+from modules import mongo_connect, validations
+from flask_wtf.csrf import CSRFProtect
 import os
 
-secret_key = os.urandom(32)
+csrf = CSRFProtect()
+# The secret_key is necessary to use csrf protection, so us created this.
+secret_key = os.urandom(24)
 
 # Instance of Flask App
-app = Flask(__name__)
-app.config['SECRET_KEY'] = secret_key
 
-# HomePage Route
-# TODO> HOME
-@app.route("/")
-def home_page():
-    form = validate.add_validate()
-    # return list with data from DataBase
-    data = mongo_connect.db_management().get_data()
-    return render_template("home.html", items=data, form=form)
+# TODO> The Initial Function.
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = secret_key
+    # To set this "csrf.init_app(app)" is necessary to add all the code inside 
+    # to this function "create_app()".
+    csrf.init_app(app)
+
+    # HomePage Route
+    # TODO> HOME
+    @app.route("/")
+    def home_page():
+        form = validations.AddValidate()
+        # return list with data from DataBase
+        data = mongo_connect.db_management().get_data()
+        return render_template("home.html", items=data, form=form)
+
+    # POST to add item in list
+    # TODO> ADD
+
+    @app.route("/add_item", methods=["POST"])
+    def add_item():
+        # class "Form" receive "formdata(dict)=request.form(dict)"
+        form = validations.AddValidate()
+        # get input from form with name.
+        # IF HTTP = POST
+        if request.method == 'POST' and form.validate_on_submit():
+            frota = form.frota.data  # request.form['frota'] > before
+            # request.form['placa'].upper() > before
+            plate = form.plate.data.upper()
+
+            print(frota, plate)
+            # append data from input in list.
+            add = mongo_connect.db_management()
+            add.add_to_db(frota=int(frota), placa=str(plate))
+
+            # returning to "home_page" after to add itens in table with the list.
+            return redirect(url_for("home_page"))
+        else:
+            print(form.errors)
+            return redirect(url_for("home_page"))
+
+    # POST to update item in list / GET to access update page.
+    # TODO> UPDATE
+    @app.route("/editar/<id>", methods=["GET", "POST"])
+    def update(id):
+        # Get specific car data.
+        item_id = mongo_connect.db_management().get_by_id(str(id))
+
+        # This is accessed from update page
+        if request.method == "POST":
+            # Get Form Data.
+            form_frota = request.form['form_frota']
+            form_plate = request.form['form_placa'].upper()
+
+            # get old data.
+            old_frota = item_id['frota']
+            old_plate = item_id['placa']
+
+            # get new data
+            new_frota = int(form_frota)
+            new_plate = form_plate
+
+            # Set update in DB
+            mongo_connect.db_management().update_data(
+                old_frota, new_frota, old_plate, new_plate)
+
+            # Redirect to page
+            return redirect(url_for("home_page"))
+
+        # This is accessed from home page
+        elif request.method == "GET":
+            # Return to update form
+            return render_template("update.html", item=item_id)
+
+    # TODO> DELETE
+
+    @app.route("/deletar/<id>", methods=["GET", "POST"])
+    def delete(id):
+        mongo_connect.db_management().delete_data(id)
+        return redirect(url_for('home_page'))
+
+    # Main execute
+    if __name__ == "__main__":
+        app.run(debug=True)
 
 
-# POST to add item in list
-# TODO> ADD
-@app.route("/add_item", methods=["POST"])
-def add_item():
-    # class "Form" receive "formdata(dict)=request.form(dict)"
-    form = validate.add_validate(request.form)
-    # get input from form with name.
-    # IF HTTP = POST
-    if request.method == 'POST' and form.validate():
-        frota = form.frota.data # request.form['frota'] > before
-        plate = form.plate.data.upper() # request.form['placa'].upper() > before
-
-        print(frota, plate)
-        # append data from input in list.
-        add = mongo_connect.db_management()
-        add.add_to_db(frota=int(frota), placa=str(plate))
-
-        # returning to "home_page" after to add itens in table with the list.
-        return redirect(url_for("home_page"))
-    else:
-        print("NÃO")
-        return redirect(url_for("home_page"))
-
-# POST to update item in list / GET to access update page.
-# TODO> UPDATE
-@app.route("/editar/<id>", methods=["GET", "POST"])
-def update(id):
-    # Get specific car data.
-    item_id = mongo_connect.db_management().get_by_id(str(id))
-
-    # This is accessed from update page
-    if request.method == "POST":
-        # Get Form Data.
-        form_frota = request.form['form_frota']
-        form_plate = request.form['form_placa'].upper()
-
-        # get old data.
-        old_frota = item_id['frota']
-        old_plate = item_id['placa']
-
-        # get new data
-        new_frota = int(form_frota)
-        new_plate = form_plate
-
-        # Set update in DB
-        mongo_connect.db_management().update_data(
-            old_frota, new_frota, old_plate, new_plate)
-
-        # Redirect to page
-        return redirect(url_for("home_page"))
-
-    # This is accessed from home page
-    elif request.method == "GET":
-        # Return to update form
-        return render_template("update.html", item=item_id)
-
-
-# TODO> DELETE
-@app.route("/deletar/<id>", methods=["GET", "POST"])
-def delete(id):
-    mongo_connect.db_management().delete_data(id)
-    return redirect(url_for('home_page'))
-
-
-# Main execute
-if __name__ == "__main__":
-    app.run(debug=True)
+create_app()
--- modules/validations.py
+++ modules/validations.py
@@ -1,11 +1,13 @@
-from wtforms import Form, StringField, PasswordField, validators, SubmitField
+from wtforms import StringField, PasswordField, validators, SubmitField
+from flask_wtf import FlaskForm
+
 
 """ Form as a parameter indicating this class "add_validate"
 is a subclass of "Form", so 'add_validate' inherits (herda)
 the attributes for class 'Form'"""
 
 
-class add_validate(Form):  # Form(formdata=request.form[is dict])
+class AddValidate(FlaskForm):
     frota = StringField("Frota", [validators.DataRequired()])
     plate = StringField(
         "Placa", [validators.DataRequired()])
